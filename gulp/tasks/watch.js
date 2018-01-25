0739028ef9110a8469376194f9a3eb946c12976d
Ian <ian.m.trent@gmail.com>
2018-01-25 18:32:40 -0500

    completed desktop and mobile header styles.

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -19,9 +19,18 @@ gulp.task('watch', function() {
     gulp.start('cssInject');
   });
 
+  watch('./app/assets/scripts/**/*.js', function(){
+    gulp.start('scriptsRefresh');
+  })
+
 });
 
 gulp.task('cssInject', ['styles'], function(){
   return gulp.src('./app/temp/styles/styles.css')
     .pipe(browserSync.stream());
 });
+
+//Refreshes the scripts when typed "gulp watch" on command line to see if there are any changes
+gulp.task('scriptsRefresh', ['scripts'], function(){
+  browserSync.reload();
+})
